3670f8458646bb9a94d2c74ab0a8504171b7a182
justinofimbert <justinofimbert@gmail.com>
2022-02-04 00:36:31 -0300

    added name.js and name.html

---
+++ name.js
@@ -0,0 +1,5 @@
+let answer = prompt('what is the "official" name of JavaScript?', "");
+
+let output = (answer === "ECMAScript") ? "yes, you know how JavaScript is really called" : "no, in reality it is called ECMAScript"
+
+alert(output)
